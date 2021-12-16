c5928840bea2f358a5ec128cacbd727590e1fe2c
Nadya <gnadyas_14@mail.ru>
2021-12-16 20:14:59 +0300

    add function MyPow()

--- funcs.h
+++ funcs.h
@@ -6,6 +6,7 @@ double MySum(double, double);
 double MySub(double, double);
 double MyMul(double, double);
 double MyDiv(double, double);
+int MyPow(int, int);
 
 #endif // __FUNCS_H__
 #pragma once
