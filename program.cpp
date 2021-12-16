01bf05cc09cf972f2ffd8bbd840e320ebac43e01
Nadya <gnadyas_14@mail.ru>
2021-12-16 19:53:12 +0300

    Snapshot3, add pause in program.cpp:

--- program.cpp
+++ program.cpp
@@ -1,18 +1,12 @@
-//τΰιλ program.cpp
+//Ρ„Π°ΠΉΠ» program.cpp
 #include "stdafx.h"
 #include <iostream>
+#include "funcs.h"
 using namespace std;
-double MySum(double a, double b)
-{
-	return a + b;
-}
-double MySub(double a, double b)
-{
-	return a - b;
-}
 int main()
 {
 	cout << MySum(10, 3) << endl;
 	cout << MySub(10, 3) << endl;
+        system("pause");
 	return 0;
-}
+}
